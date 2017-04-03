79693f62becc78f375ee8b94ef2049db426144bc
stefan <stefan@illutek.be>
2017-04-03 13:26:49 +0200

    Modal werkende
    algemene voorwaarden in overlay btn op form vervoer-aanvraag

--- js/custom-modal.js
+++ js/custom-modal.js
@@ -1,6 +1,32 @@
 /**
  * Created by Stefan on 2/04/2017.
  */
-$(document).ready(function () {
+jQuery(document).ready(function($) {
+    $('.conditions').click(function () {
+        openModal();
+    });
 
+    $('.close-button').click(function () {
+        closeModal();
+    });
+
+    $(window).keypress(function (event) {
+        if (event.which == 27) {
+            closeModal();
+        }
+    });
+
+    $('.modal-container').click(function (event) {
+        if (this == event.target) {
+            closeModal();
+        }
+    });
 });
+
+function openModal() {
+    jQuery('.modal-container').addClass('modal-open');
+}
+
+function closeModal() {
+    jQuery('.modal-container').removeClass('modal-open');
+}
