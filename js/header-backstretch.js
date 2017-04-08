08047f32ca6fc3b1cdfa8af58e758e92e82f290e
stefan <stefan@illutek.be>
2017-04-08 20:40:36 +0200

    page-front tiles enkel op front

--- js/header-backstretch.js
+++ js/header-backstretch.js
@@ -3,7 +3,7 @@
  */
 jQuery(document).ready(function($) {
     var base = drupalSettings.path.baseUrl + 'themes/custom/degroote/';
-    $("header").backstretch([
+    $(".header-front").backstretch([
         base + "images/materiaal-3-min.jpg",
         base + "images/materiaal-2-min.jpg",
         base + "images/materiaal-1-min.jpg"
